e4d5edcb88af135751218a88ee826d81352531d2
Mathias Magnusson <mathias@magnusson.space>
2025-09-08 14:40:18 +0200

    make it click very nicely

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 import postgres from "postgres";
 import polka from "polka";
+import fs from "fs/promises";
 
 const sql = postgres({});
 
@@ -11,11 +12,24 @@ await sql`
     insert into count (value) values (0);
 `.simple();
 
+const index = await fs.readFile("index.html", { encoding: "utf-8" });
+
 polka()
     .get("/", async (req, res) => {
         const [count] = await sql`
             select value from count;
         `;
-        res.end(`<h1>${count.value}</h1>`);
+        res.end(index.replaceAll("$COUNT$", count.value));
+    })
+    .post("/", async (req, res) => {
+        await sql`
+            update count
+            set value = value + 1;
+        `;
+        res.writeHead(303, {
+            'Location': "/",
+            'Content-Length': 0
+        });
+        res.end();
     })
     .listen(80);
